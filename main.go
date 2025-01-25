64e190663ee1252f44f478f883b001f4fabf117e
Allen Suvorov <allen.suvorov@gmail.com>
2025-01-24 19:35:55 -0800

    Add a snippetCreate handler function and register the new handler function with corresponding route pattern with the servemux

--- main.go
+++ main.go
@@ -13,10 +13,16 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Print("starting server on :4000")
 
 	log.Fatal(http.ListenAndServe(":4000", mux))

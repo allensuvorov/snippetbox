db2e03a4955b9cbf90b295487e701c0bdaf0479c
Allen Suvorov <allen.suvorov@gmail.com>
2025-01-24 18:33:04 -0800

    add handler home

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
 	fmt.Println("Hello world!")

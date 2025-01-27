73734d39f654fab1513dd7513f7c81c1572561a8
Allen Suvorov <allen.suvorov@gmail.com>
2025-01-26 21:24:27 -0800

    write template as response body: * Use Execute() method on the template set to write the template content as the response body. * The last parameter to Execute() represents any dynamic data we want to pass in, which for now we'll leave as nil.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,6 +17,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {

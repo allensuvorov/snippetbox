38828102cf0edd4c3e357153ded7570945f7f538
Allen Suvorov <allen.suvorov@gmail.com>
2025-01-28 19:07:31 -0800

    Update path to static to make it go from the root directory

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,10 +12,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
 	files := []string{
-		"../../ui/html/base.html",
-		"../../ui/html/partials/nav.html",
-		"../../ui/html/pages/home.html",
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
 	}
+
+	// files := []string{
+	// 	"../../ui/html/base.html",
+	// 	"../../ui/html/partials/nav.html",
+	// 	"../../ui/html/pages/home.html",
+	// }
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {

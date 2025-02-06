0413dce697996f983271fec991dc9c80b60f8d21
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-05 18:04:39 -0800

    Use the QueryRow() method of the connection pool to execute our SQL statement * passing in the untrusted id variable as the value for the placeholder parameter * This returns a pointer to a sql.Row object which holds the result from the ddatabase.

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -37,6 +37,9 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 func (m *SnippetModel) Get(id int) (Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
 	return Snippet{}, nil
 }
 

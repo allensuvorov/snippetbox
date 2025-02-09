47a68bf1bb4e946e980558c484e7b0a7e515e70e
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-08 19:53:48 -0800

    Include the Snippets field in the templateData struct.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/allensuvorov/snippetbox.git/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }

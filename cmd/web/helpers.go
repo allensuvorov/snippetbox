5e2af3b3582805eb73448729961c7c80a16caa90
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-09 20:07:44 -0800

    Add render() method to helpers.go: * to remove duplicate code * to easily render the templates form the cache

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -19,3 +19,7 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
+
+}

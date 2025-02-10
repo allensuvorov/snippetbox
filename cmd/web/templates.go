65747b48c7635b5a2e52f654165ec075fe70a53c
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-09 21:32:18 -0800

    Parse the base template file into the template set.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -16,7 +16,6 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
-
 	if err != nil {
 		return nil, err
 	}
@@ -24,16 +23,21 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		files := []string{
-			"./ui/html/base.html",
-			"./ui/html/partials/nav.html",
-			page,
-		}
-
-		ts, err := template.ParseFiles(files...)
+		ts, err := template.ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
+
+		// files := []string{
+		// 	"./ui/html/base.html",
+		// 	"./ui/html/partials/nav.html",
+		// 	page,
+		// }
+
+		// ts, err = template.ParseFiles(files...)
+		// if err != nil {
+		// 	return nil, err
+		// }
 
 		cache[name] = ts
 	}

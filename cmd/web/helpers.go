3114991b32d241a4a38a588d76da3f6fb428137a
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-09 21:26:44 -0800

    Fix path in templates.go. Remove stack tracing in logging.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"runtime/debug"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
 		method = r.Method
 		uri    = r.URL.RequestURI()
-		trace  = string(debug.Stack())
+		// trace  = string(debug.Stack())
 	)
 
-	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	// app.logger.Error(err.Error(), "method", method, "uri", uri , "trace", trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
@@ -29,7 +29,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		return
 	}
 
-	w.WriteHeaders(status)
+	w.WriteHeader(status)
 
 	err := ts.ExecuteTemplate(w, "base", data)
 	if err != nil {

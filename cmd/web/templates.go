489979b369e2e284ffb10acd04757dbbfe502ac9
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-10 19:26:42 -0800

    Use template.New() to create an empty template set, use the Funcs() method to register the template.FuncMap and then parse the file as normal. * The template.FuncMap must be registered with the template set before you call the ParseFiles() method.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -33,7 +33,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}

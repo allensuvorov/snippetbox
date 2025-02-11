2c7ac2d44a5499d77224481ff341ecbd3650e6a6
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-10 18:37:23 -0800

    Call the newTemplateData() to get a templateData struct containing the 'default' data (which for now is just the current year), and add the snippets slice to it.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,9 +18,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "home.html", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	// app.render(w, r, http.StatusOK, "home.html", templateData{
+	// 	Snippets: snippets,
+	// })
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {

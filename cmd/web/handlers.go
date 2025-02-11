101bfdc49e3964fafa6a8dacc9aeb99e5f1e0502
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-10 18:40:00 -0800

    Pass the data to the render() helper as normal.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,9 +21,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	data.Snippets = snippets
 
-	// app.render(w, r, http.StatusOK, "home.html", templateData{
-	// 	Snippets: snippets,
-	// })
+	app.render(w, r, http.StatusOK, "home.html", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {

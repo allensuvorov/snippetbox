3776bb0239a290d105ba73db2182c5d03dce6b92
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-11 17:59:25 -0800

    Set headers Referrer-Policy to control what info is included in a Referer header whne a user navigates away from you web page.

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -5,5 +5,6 @@ import "net/http"
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 	})
 }

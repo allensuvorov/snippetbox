d4ccde6b91d8d8c55fd6add16146547759a1668a
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-11 18:01:20 -0800

    Set header X-Frame-Options: deny to prevent clickjacking attacks.

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -7,5 +7,6 @@ func commonHeaders(next http.Handler) http.Handler {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
 	})
 }

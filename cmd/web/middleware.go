b1bc6688298a57eedeb7bbc1181c097f5cf8ff1c
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-11 17:57:54 -0800

    Set headers Content-Security-Policy

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,6 +4,6 @@ import "net/http"
 
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 	})
 }

817802b4aa74def5598898771570863390e98183
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-12 17:44:07 -0800

    Set a "Connection: close" header on the response.

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -35,7 +35,7 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func () {
 			if err := recover(); err != nil {
-
+				w.Header().Set("Connection", "close")
 			}
 		}
 		next.ServeHTTP(w, r)

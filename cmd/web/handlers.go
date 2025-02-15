e35e41c6d557d6fa0e2086da56cfb50736d5e773
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-14 19:22:37 -0800

    Update the validation checks so that they operate on the snippetCreateForm instance.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -81,18 +81,18 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		FieldErrors: make(map[string]string),
 	}
 
-	if strings.TrimSpace(title) == "" {
-		fieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		fieldErrors["title"] = "this field cannot be more than 100 characters long"
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "this field cannot be more than 100 characters long"
 	}
 
-	if strings.TrimSpace(content) == "" {
-		fieldErrors["content"] = "This field cannot be blank"
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
 	}
 
-	if expires != 1 && expires != 7 && expires != 365 {
-		fieldErrors["expires"] = "This field must equal 1, 7, or 365"
+	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7, or 365"
 	}
 
 	if len(fieldErrors) > 0 {

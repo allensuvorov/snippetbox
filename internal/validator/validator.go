2eadf76c45011a41247a7d91bc5f3320f6a4a173
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-15 18:39:59 -0800

    NotBlank() returns true if a value is not an empty string.

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,5 +1,7 @@
 package validator
 
+import "strings"
+
 type Validator struct {
 	FieldErrors map[string]string
 }
@@ -22,4 +24,8 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 	if !ok {
 		v.AddFieldError(key, message)
 	}
+}
+
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
 }

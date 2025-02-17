4a1c6e5224f05cc6066514b31a05a65431c12924
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-16 17:15:28 -0800

    Add a formDecoder field to hold a pointer to a form.Decoder instance.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"text/template"
 
 	"github.com/allensuvorov/snippetbox.git/internal/models"
+	"github.com/go-playground/form/v4"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -17,6 +18,7 @@ type application struct {
 	logger        *slog.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {

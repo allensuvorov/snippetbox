bae675a528a33fb233626416c2c209a57a11170c
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-17 18:02:15 -0800

    Updat these routes to use the new dynamic middleware chain followed by the appropriate handler function. * Note that because the alice ThenFunc() method returns an http.Handler (rather than an http.HandlerFunc) we also need to switch to registering the route using the mux.Handle() method.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,10 +14,10 @@ func (app *application) routes() http.Handler {
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 

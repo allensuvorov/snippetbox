07a6c7ec58c0e69d0880a1e5ead4c8d47861126f
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-18 17:48:06 -0800

    To start HTTPS web server set Secure attribute on session cookies: * Make sure that the Secure attribute is set on our session cookies. * Setting this means that the cookie will only be sent by a user's web browser when a HTTPS connection is being used (won't be sent over an unsecure HTTP connection).

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -53,6 +53,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		logger:         logger,

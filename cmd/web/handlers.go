8eb1f34e190304b904635edec345c56ce5efedb6
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-20 19:39:05 -0800

    Add a placeholder for a new handler function userSignupPost(). * It will create a new user.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -99,3 +99,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Display a form for signing up a new user ...")
 }
+
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}

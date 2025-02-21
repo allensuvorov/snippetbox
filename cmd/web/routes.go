0e1acc38bf19cb8346646491148b08fef436682b
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-20 20:10:33 -0800

    Add route 'POST /user/login'.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,6 +22,7 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 

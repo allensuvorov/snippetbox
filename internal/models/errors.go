5e62cf328ac9f3eda933a6952435b6466023e4e4
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-21 16:56:37 -0800

    Add a new ErrDuplicateEmail error. * We'll use this later if a user tries to signup with an email address that's already in use.

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrNoRecord           = errors.New("models: no matching record found")
 	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
 )

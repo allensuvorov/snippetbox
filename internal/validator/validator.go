d70eecec441d8a8435ebba7562f434cd27dfb9cf
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-23 12:58:52 -0800

    Use the regexp.MustCompile() function to parse a regular expression pattern for sanity checking for format of an email address. * This reterns a pointer to a 'compiled' regexp.Regexp type, or panics in the event of an error. * Parsing this pettern once at startup and storing the compiled *regexp.Regexp in a variable is more performant than re-parsing the pattern each time we need it.

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,10 +1,13 @@
 package validator
 
 import (
+	"regexp"
 	"slices"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
 	FieldErrors map[string]string

a991b95eb30a183514e08b44f0b84cc64c3a1bd7
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-28 18:57:44 -0800

    Retrieve the id and hashed password associated with the given email. * If no matching email exists, we return the ErrInvalidCredentials error.

--- internal/models/users.go
+++ internal/models/users.go
@@ -44,6 +44,20 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE EMAIL = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
 	return 0, nil
 }
 

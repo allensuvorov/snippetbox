a281034d1d67ecc9e16169541f5969a45dedefb9
Allen Suvorov <allen.suvorov@gmail.com>
2025-02-28 19:02:44 -0800

    Check whether the hashed password and plain-text password provided match. * If the don't, we return the ErrInvalidCredentials error.

--- internal/models/users.go
+++ internal/models/users.go
@@ -58,6 +58,14 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 		}
 	}
 
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
 	return 0, nil
 }
 

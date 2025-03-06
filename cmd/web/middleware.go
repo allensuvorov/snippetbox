dadf81377cbc9dc2afcdd3ba27a6e3de175077ea
Allen Suvorov <allen.suvorov@gmail.com>
2025-03-05 19:52:39 -0800

    Add requireAuthentication middleware function. * We need to make sure that if an unauthenticated user tries to visit any routes with the URL path /snippet/create they are redirected to /user/login instead.

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -45,3 +45,9 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}

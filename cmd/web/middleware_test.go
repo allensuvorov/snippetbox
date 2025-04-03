b0f2a11f19863812ea6f00499c03bca14dd1133e
Allen Suvorov <allen.suvorov@gmail.com>
2025-04-02 18:13:44 -0700

    Pass the mock HTTP hander to our commonHeader middleware. * Because commonHeader *returns* an http.Handler we can call it's ServeHTTP() method, passing in the http.ResponseRecorder and dummy http.Request to execute it.

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -17,4 +17,6 @@ func TestCommonHeaders(t *testing.T) {
 	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
+
+	commonHeaders(next).ServeHTTP(rr, r)
 }

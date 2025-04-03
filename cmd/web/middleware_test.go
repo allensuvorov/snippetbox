24370460112d0c730e68f0a7760a6825023eeb73
Allen Suvorov <allen.suvorov@gmail.com>
2025-04-02 18:34:21 -0700

    Check response body.

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -43,4 +45,13 @@ func TestCommonHeaders(t *testing.T) {
 	assert.Equal(t, rs.Header.Get("Server"), expectedValue)
 
 	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
 }

975e2d1a7e75d505816cbec523f600fd3c14ebf8
Allen Suvorov <allen.suvorov@gmail.com>
2025-04-02 18:11:55 -0700

    Create a mock HTTP hander that we can pass to our commonHeaders middleware, which writes a 200 status code and an "OK" response body.

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -8,9 +8,13 @@ import (
 
 func TestCommonHeaders(t *testing.T) {
 	rr := httptest.NewRecorder()
+
 	r, err := http.NewRequest(http.MethodGet, "/", nil)
 	if err != nil {
 		t.Fatal(err)
-
 	}
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
 }

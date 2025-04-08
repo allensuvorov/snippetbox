4322826d2abf34bdee770084472d1922a469e990
Allen Suvorov <allen.suvorov@gmail.com>
2025-04-07 20:26:27 -0700

    Define a custom testServer type which embeds a httptest.Server instance.

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"log/slog"
+	"net/http/httptest"
 	"testing"
 )
 
 func newTestApplication(t *testing.T) *application {
 	return &application{
 		logger: slog.New(slog.DiscardHandler),
+	}
+
+	type testServer struct {
+		*httptest.Server
 	}
 }

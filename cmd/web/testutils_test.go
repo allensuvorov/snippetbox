5bbd5d0867bb0d8f45568d62f3a1f90527193924
Allen Suvorov <allen.suvorov@gmail.com>
2025-04-07 20:33:18 -0700

    Implement a get() method on our custom testServer type. * This makes a GET request to a given url path using the test server client, and returns the response status code, headers and body.

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
@@ -20,4 +22,20 @@ type testServer struct {
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
 	return &testServer{ts}
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
 }
